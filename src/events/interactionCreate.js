319e33a2744190d637937eee26dcd692031567af
5xp <62446202+5xp@users.noreply.github.com>
2023-01-13 05:11:35 -0600

    Bot skeleton

---
+++ src/events/interactionCreate.js
@@ -0,0 +1,23 @@
+const { Events } = require("discord.js");
+
+module.exports = {
+  name: Events.InteractionCreate,
+  once: false,
+  async execute(interaction) {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+      console.error(`Command ${interaction.commandName} not found.`);
+      return;
+    }
+
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+    }
+  },
+};
