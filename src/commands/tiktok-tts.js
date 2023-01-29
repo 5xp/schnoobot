68bc0fb0e9f68525a9e633a03805ef8569758f76
chris <62446202+5xp@users.noreply.github.com>
2023-01-28 19:18:18 -0600

    Remove duplicated tts code

--- src/commands/tiktok-tts.js
+++ src/commands/tiktok-tts.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, AttachmentBuilder, bold } = require("discord.js");
+const { SlashCommandBuilder, AttachmentBuilder, bold, blockQuote, escapeMarkdown } = require("discord.js");
 const TikTokTTS = require("../utils/tiktok-util");
 
 module.exports = {
@@ -16,20 +16,29 @@ module.exports = {
     .addBooleanOption(option =>
       option.setName("ephemeral").setDescription("Whether the response should be ephemeral.").setRequired(false),
     ),
-  async execute(interaction) {
+  async execute(interaction, message = null, voice = null, ephemeral = null) {
     if (!process.env.TIKTOK_SESSIONID) {
       return interaction.reply({ content: bold("TikTok session ID not set!"), ephemeral: true });
     }
 
-    const message = interaction.options.getString("message");
-    const voice = interaction.options.getString("voice");
-    const ephemeral = interaction.options.getBoolean("ephemeral") ?? false;
+    message ??= interaction.options.getString("message");
+    voice ??= interaction.options.getString("voice");
+    ephemeral ??= interaction.options.getBoolean("ephemeral") ?? false;
+
+    if (!interaction.replied && !interaction.deferred) {
+      interaction.deferReply({ ephemeral });
+    }
 
     const base64 = await TikTokTTS.getTTSBase64(voice, message);
     const buffer = Buffer.from(base64, "base64");
 
     const attachment = new AttachmentBuilder(buffer, { name: "tts.mp3" });
 
-    await interaction.reply({ files: [attachment], ephemeral });
+    await interaction.editReply({
+      content: blockQuote(escapeMarkdown(message)),
+      files: [attachment],
+      components: [],
+      ephemeral,
+    });
   },
 };
--- src/commands/tiktok-tts.menu.js
+++ src/commands/tiktok-tts.menu.js
@@ -2,12 +2,11 @@ const {
   ContextMenuCommandBuilder,
   ApplicationCommandType,
   bold,
-  AttachmentBuilder,
   StringSelectMenuBuilder,
   ActionRowBuilder,
-  blockQuote,
 } = require("discord.js");
 const TikTokTTS = require("../utils/tiktok-util");
+const { execute } = require("./tiktok-tts");
 
 async function selectVoice(interaction) {
   const voiceSelect = new StringSelectMenuBuilder()
@@ -43,24 +42,10 @@ module.exports = {
   async execute(interaction) {
     const message = interaction.targetMessage;
     const content = message.cleanContent;
-
-    if (!content) {
-      return interaction.reply({ content: bold("There is no text to convert to speech!"), ephemeral: true });
-    }
-
-    if (!process.env.TIKTOK_SESSIONID) {
-      return interaction.reply({ content: bold("TikTok session ID not set!"), ephemeral: true });
-    }
-
     const voice = await selectVoice(interaction);
 
     if (!voice) return;
 
-    const base64 = await TikTokTTS.getTTSBase64(voice, content);
-    const buffer = Buffer.from(base64, "base64");
-
-    const attachment = new AttachmentBuilder(buffer, { name: "tts.mp3" });
-
-    await interaction.editReply({ content: blockQuote(content), files: [attachment], components: [] });
+    execute(interaction, content, voice, false);
   },
 };
